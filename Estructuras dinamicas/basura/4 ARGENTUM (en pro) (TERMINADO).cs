a995d678a4d8c8287a325b34a4c2064c6cb32bed
CRUFTY <100171168+CRUFTYY@users.noreply.github.com>
2022-11-04 00:16:04 -0300

    Add files via upload

---
+++ Estructuras dinamicas/basura/4 ARGENTUM (en pro) (TERMINADO).cs
@@ -0,0 +1,303 @@
+﻿using System;
+
+namespace ConsoleApp82
+{
+    internal class Program
+    {
+
+        public struct leer
+        {
+            public string status;
+            public string nom;
+
+            public int criminales;
+            public int ciudadanos;
+
+        }
+        public struct armada
+        {
+            public string nom;
+
+            public int hcaballo;
+            public int arqueria;
+            public int navegacion;
+        }
+        public struct legion
+        {
+            public string nom;
+
+            public int poder;
+            public int fuerza;
+            public int celeridad;
+
+        }
+        static void lee(leer[] leer, armada[] armada, legion[] legion, int cant)
+        {
+            for (int i = 0; i < cant; i++)
+            {
+                Console.WriteLine("Ingrese nombre del personaje");
+                leer[i].nom = Console.ReadLine();
+
+                Console.WriteLine("Ingrese cantidad de criminales asesinados");
+                leer[i].criminales = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese cantidad de ciudadanos asesinados");
+                leer[i].ciudadanos = int.Parse(Console.ReadLine());
+            }
+        }
+        static void faccionpert(leer[] leer, armada[] armada, legion[] legion, int cant)
+        {
+            for (int i = 0; i < cant; i++)
+            {
+                if (leer[i].criminales > 150 && leer[i].ciudadanos == 0)
+                {
+                    leer[i].status = "armada";
+                    Console.WriteLine("");
+                    Console.WriteLine("El personaje " + armada[i].nom + "  pertenece a la faccion la   Armada Real");
+                    Console.WriteLine("");
+                }
+                if (leer[i].ciudadanos > 200)
+                {
+                    leer[i].status = "legion";
+
+                    legion[i].nom = leer[i].nom;
+                    Console.WriteLine("");
+                    Console.WriteLine("El personaje " + legion[i].nom + "  pertenece a la faccion la   Legión Oscura");
+                    Console.WriteLine("");
+                }
+                if (leer[i].ciudadanos > 0 && leer[i].ciudadanos< 200 && leer[i].criminales < 150)
+                {
+                    Console.WriteLine(leer[i].nom + "  No pertenece a ninguna facción");
+                }
+            }
+        }
+        static void Cualidade(leer[] leer, armada[] armada, legion[] legion, int cant)
+        {
+
+                for (int h = 0; h < cant; h++)
+                {
+                    if (leer[h].status == "legion")
+                    {
+
+                        legion[h].nom = leer[h].nom;
+
+                        Console.WriteLine("Ingrese poder de habilidad del personaje : " + legion[h].nom);
+                        legion[h].poder = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Ingrese fuerza del personaje :    " + legion[h].nom);
+                        legion[h].fuerza = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Ingrese celeridad del personaje : " + legion[h].nom);
+                        legion[h].celeridad = int.Parse(Console.ReadLine());
+
+                    }
+                    if (leer[h].status == "armada")
+                    {
+
+                            armada[h].nom = leer[h].nom;
+
+                            Console.WriteLine("Ingrese habilidades a caballo del personaje : " + armada[h].nom);
+                            armada[h].hcaballo = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Ingrese arqueria del personaje : " + armada[h].nom);
+                            armada[h].arqueria = int.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Ingrese navegacion del personaje : " + armada[h].nom);
+                            armada[h].navegacion = int.Parse(Console.ReadLine());
+
+                        
+                    }
+                }
+            
+        }
+        static void aumentoarmada(leer[] leer, armada[] armada, legion[] legion, int cant)
+        {
+            for (int i = 0; i < cant; i++)
+            {
+                if (leer[i].status == "armada")
+                {
+                    
+                    armada[i].hcaballo = armada[i].hcaballo + armada[i].hcaballo * 25 / 100;
+                    armada[i].arqueria = armada[i].arqueria + armada[i].arqueria * 25 / 100;
+                    armada[i].navegacion = armada[i].navegacion + armada[i].navegacion * 25 / 100;
+                    Console.WriteLine("");
+                    Console.WriteLine("  AUMENTO DE ARMADA  ");
+                    Console.WriteLine("");
+                    Console.WriteLine("Se aumento 25 % a sus habilidades a caballo, arquería y navegación");
+                    Console.WriteLine("Por ende despues del aumento los valores serian los siguientes : ");
+                    Console.WriteLine("");
+                    Console.WriteLine("Nombre : " + armada[i].nom);
+                    Console.WriteLine("habilidades a caballo : " + armada[i].hcaballo);
+                    Console.WriteLine("Arqueria  :  " + armada[i].arqueria);
+                    Console.WriteLine("Navegacion : " + armada[i].navegacion);
+                    Console.WriteLine("");
+                    
+                }
+            }
+        }
+        static void aumentolegion(leer[] leer, armada[] armada, legion[] legion, int cant)
+        {
+            for (int i = 0; i < cant; i++)
+            {
+                if (leer[i].status == "legion")
+                {
+                    legion[i].fuerza = legion[i].fuerza + legion[i].fuerza * 20 / 100;
+                    legion[i].celeridad = legion[i].celeridad + legion[i].celeridad * 20 / 100;
+                    legion[i].poder = legion[i].poder + legion[i].poder * 20 / 100;
+
+                    Console.WriteLine("");
+                    Console.WriteLine("   AUMENTO DE LEGION   ");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("Se aumento 20 % a sus poder de habilidad, fuerza y celeridad");
+                    Console.WriteLine("Por ende despues del aumento los valores serian los siguientes : ");
+                    Console.WriteLine("");
+                    Console.WriteLine("Nombre : " + legion[i].nom);
+                    Console.WriteLine("poder de habilidad : " + legion[i].poder);
+                    Console.WriteLine("fuerza  :  " + legion[i].fuerza);
+                    Console.WriteLine("celeridad : " + legion[i].celeridad);
+                    Console.WriteLine("");
+                }
+            }
+        }
+        static void final(leer[] leer, armada[] armada, legion[] legion, int cant)
+        {
+            int legionsum = 0;
+            int armadasum = 0;
+
+            for (int i = 0; i < cant; i++)
+            {
+                legionsum = legion[i].poder + legion[i].fuerza + legion[i].celeridad ;
+                armadasum = armada[i].hcaballo + armada[i].arqueria + armada[i].navegacion;
+            }
+
+            for (int l = 0; l < cant; l++)
+            {
+                if (armadasum > legionsum && leer[l].status == "armada")
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Gano  ARMADA REAL ");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+
+                        armada[l].hcaballo = armada[l].hcaballo + armada[l].hcaballo * 30 / 100;
+                        armada[l].arqueria = armada[l].arqueria + armada[l].arqueria * 30 / 100;
+                        armada[l].navegacion = armada[l].navegacion + armada[l].navegacion * 30 / 100;
+
+                        Console.WriteLine("");
+                        Console.WriteLine("NOMBRE : " + armada[l].nom);
+                        Console.WriteLine("");
+                        Console.WriteLine("Habilidades a caballo : " + armada[l].hcaballo);
+                        Console.WriteLine("Arqueria : " + armada[l].arqueria);
+                        Console.WriteLine("Navegacion : " + armada[l].navegacion);
+                        Console.WriteLine("");
+
+
+                }
+
+                if (legionsum > armadasum && leer[l].status == "legion")
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Gano  LEGION OSCURA ");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+
+                        legion[l].fuerza = legion[l].fuerza + legion[l].fuerza * 30 / 100;
+                        legion[l].poder = legion[l].fuerza + legion[l].fuerza * 30 / 100;
+                        legion[l].celeridad = legion[l].celeridad + legion[l].celeridad * 30 / 100;
+                       
+
+                        Console.WriteLine("NOMBRE : " + legion[l].nom);
+                        Console.WriteLine("Poder de habilidad : " + legion[l].poder);
+                        Console.WriteLine("Fuerza  :  " + legion[l].fuerza);
+                        Console.WriteLine("Celeridad : " + legion[l].celeridad);
+
+                }
+                if (legionsum == armadasum)
+                {
+
+                        Console.WriteLine("Empate. Los equipos empataron , por lo tanto no se aumenta nada. No hay ganador");
+                        Console.WriteLine("");
+
+                            Console.WriteLine("");
+                            Console.WriteLine("Habilidades a caballo : " + armada[l].hcaballo);
+                            Console.WriteLine("Arqueria : " + armada[l].arqueria);
+                            Console.WriteLine("Navegacion : " + armada[l].navegacion);
+                            Console.WriteLine("");
+                        
+
+                            Console.WriteLine("");
+                            Console.WriteLine("Poder de habilidad :  " + legion[l].poder);
+                            Console.WriteLine("Fuerza  :  " + legion[l].fuerza);
+                            Console.WriteLine("Celeridad : " + legion[l].celeridad);
+                            Console.WriteLine("");
+                }
+
+            }
+        }
+            static void Main(string[] args)
+                {
+
+                    int cant;
+                    int op;
+
+                    Console.WriteLine("Ingrese la cantidad de personajes");
+                    cant = int.Parse(Console.ReadLine());
+
+                    leer[] leer = new leer[cant];
+                    armada[] armada = new armada[cant];
+                    legion[] legion = new legion[cant];
+
+                    lee(leer, armada, legion, cant);
+                    do
+                    {
+                        Console.WriteLine("1. Facción a la que pertenece");
+                        Console.WriteLine("2. Asignar Cualidades");
+                        Console.WriteLine("3. Aumento Armadas Reales");
+                        Console.WriteLine("4. Aumento Legiones");
+                        Console.WriteLine("5. Encuentro Final");
+                        Console.WriteLine("");
+                        Console.WriteLine("6. Salir");
+                        Console.WriteLine("Ingrese una opción");
+                        op = int.Parse(Console.ReadLine());
+                        switch (op)
+                        {
+                            case 1:
+                                Console.Clear();
+                                faccionpert(leer, armada, legion, cant);
+                                break;
+                            case 2:
+                                Console.Clear();
+                                Cualidade(leer, armada, legion, cant);
+                                break;
+                            case 3:
+                                Console.Clear();
+                                aumentoarmada(leer, armada, legion, cant);
+                                break;
+                            case 4:
+                                Console.Clear();
+                                aumentolegion(leer, armada, legion, cant);
+                                break;
+                            case 5:
+                                Console.Clear();
+                                final(leer, armada, legion, cant);
+                                break;
+                            case 6:
+                                Console.Clear();
+                                Console.WriteLine("Usted eligio la opcion salir");
+
+                                break;
+                            default:
+                                Console.Clear();
+                                Console.WriteLine("");
+                                Console.WriteLine("Esta opcion es invalida , ingrese otro valor ");
+                                Console.WriteLine("");
+                                break;
+                        }
+                    } while (op != 6);
+                }
+            }        
+        }
+    
+
---
+++ Estructuras dinamicas/basura/Emp local host(Ej).cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public struct trabajadores
+        {
+            public string nomrh;
+
+            public string aprh;
+
+            public string nomv;
+
+            public string apv;
+
+            public int hr;
+
+            public int vent;
+
+            public int idrh;
+
+            public int idv;
+
+            public char sec;
+        }
+
+        static void info(trabajadores[] a, int cant)
+        {
+            for (int i = 0; i < cant; i++)
+            {
+                Console.WriteLine("Ingrese las horas trabajadas del empleado " + (i + 1));
+                a[i].hr = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Ingrese el sector del empleado " + (i + 1));
+                a[i].sec = Convert.ToChar(Console.ReadLine());
+                if (a[i].sec == 'h')
+                {
+                    Console.WriteLine("Ingrese el id del empleado " + (i + 1));
+                    a[i].idrh = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Ingrese el nombre del empleado " + (i + 1));
+                    a[i].nomrh = Convert.ToString(Console.ReadLine());
+
+                    Console.WriteLine("Ingrese el apellido del empleado " + (i + 1));
+                    a[i].aprh = Convert.ToString(Console.ReadLine());
+                }
+                else if (a[i].sec == 'v')
+                {
+                    Console.WriteLine("Ingrese el id del empleado " + (i + 1));
+                    a[i].idv = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Ingrese el nombre del empleado " + (i + 1));
+                    a[i].nomv = Convert.ToString(Console.ReadLine());
+
+                    Console.WriteLine("Ingrese el apellido del empleado " + (i + 1));
+                    a[i].apv = Convert.ToString(Console.ReadLine());
+
+                    Console.WriteLine("Ingrese las ventas del empleado " + (i + 1));
+                    a[i].vent = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese un sector valido");
+                }
+
+            }
+        }
+        static void OrdenarSector(tr[] a, int cant)
+        {
+            string aux1, aux2;
+            int aux3, aux4, aux5;
+
+            for (int i1 = 1; i1 < cant; i1++)
+            {
+                for (int i2 = 0; i2 < cant - 1; i2++)
+                {
+                    if (a[i2].idrh > a[i2 + 1].idrh)
+                    {
+                        aux1 = a[i2].nomrh;
+                        a[i2].nomrh = a[i2 + 1].nomrh;
+                        a[i2 + 1].nomrh = aux1;
+
+                        aux2 = a[i2].aprh;
+                        a[i2].aprh = a[i2 + 1].aprh;
+                        a[i2 + 1].aprh = aux2;
+
+                        aux3 = a[i2].hr;
+                        a[i2].hr = a[i2 + 1].hr;
+                        a[i2 + 1].hr = aux3;
+
+                        aux5 = a[i2].idrh;
+                        a[i2].idrh = a[i2 + 1].idrh;
+                        a[i2 + 1].idrh = aux5;
+                    }
+                }
+            }
+
+            Console.WriteLine("Recursos Humanos");
+            Console.WriteLine("");
+
+            for (int i = 0; i < cant; i++)
+            {
+                if (a[i].idrh == 0)
+                {
+                }
+                else
+                {
+                    Console.WriteLine("Nombre: " + a[i].nomrh);
+                    Console.WriteLine("Apellido: " + a[i].aprh);
+                    Console.WriteLine("ID: " + a[i].idrh);
+                    Console.WriteLine("Horas Trabajadas: " + a[i].hr);
+                    Console.WriteLine("");
+                }
+            }
+
+            for (int i1 = 1; i1 < cant; i1++)
+            {
+                for (int i2 = 0; i2 < cant - 1; i2++)
+                {
+                    if (a[i2].idv > a[i2 + 1].idv)
+                    {
+                        aux1 = a[i2].nomv;
+                        a[i2].nomv = a[i2 + 1].nomv;
+                        a[i2 + 1].nomv = aux1;
+
+                        aux2 = a[i2].apv;
+                        a[i2].apv = a[i2 + 1].apv;
+                        a[i2 + 1].apv = aux2;
+
+                        aux3 = a[i2].hr;
+                        a[i2].hr = a[i2 + 1].hr;
+                        a[i2 + 1].hr = aux3;
+
+                        aux4 = a[i2].vent;
+                        a[i2].vent = a[i2 + 1].vent;
+                        a[i2 + 1].vent = aux4;
+
+                        aux5 = a[i2].idv;
+                        a[i2].idv = a[i2 + 1].idv;
+                        a[i2 + 1].idv = aux5;
+                    }
+                }
+            }
+
+            Console.WriteLine("Ventas");
+            Console.WriteLine("");
+
+            for (int i = 0; i < cant; i++)
+            {
+                if (a[i].idv == 0)
+                {
+                }
+                else
+                {
+                    Console.WriteLine("Nombre: " + a[i].nomv);
+                    Console.WriteLine("Apellido: " + a[i].apv);
+                    Console.WriteLine("ID: " + a[i].idv);
+                    Console.WriteLine("Ventas: " + a[i].vent);
+                    Console.WriteLine("Horas Trabajadas: " + a[i].hr);
+                    Console.WriteLine("");
+                }
+            }
+        }
+
+        static void MejorEmp(trabajadores[] a, int cant)
+        {
+            int ma = a[0].hr;
+            int posmayor = 0;
+
+            for (int i = 0; i < cant; i++)
+            {
+                if (ma < a[i].hr)
+                {
+                    ma = a[i].hr;
+                    posmayor = i;
+                }
+            }
+            if (a[posmayor].idrh == 0)
+            {
+                Console.WriteLine("Nombre: " + a[posma].nomv);
+                Console.WriteLine("Apellido: " + a[posmayor].apv);
+                Console.WriteLine("ID: " + a[posmayor].idv);
+                Console.WriteLine("Ventas: " + a[posmayor].vent);
+                Console.WriteLine("Horas Trabajadas: " + a[posmayor].hr);
+                Console.WriteLine("Sector: Ventas");
+            }
+            else
+            {
+                Console.WriteLine("Nombre: " + a[posmayor].nomrh);
+                Console.WriteLine("Apellido: " + a[posmayor].aprh);
+                Console.WriteLine("ID: " + a[posmayor].idrh);
+                Console.WriteLine("Horas Trabajadas: " + a[posmayor].hr);
+                Console.WriteLine("Sector: Recursos Humanos");
+            }
+        }
+
+        static void VentasTot(trabajadores[] a, int cant)
+        {
+            int venttot = 0;
+
+            for (int i = 0; i < cant; i++)
+            {
+                venttot = venttot + a[i].vent;
+            }
+
+            Console.WriteLine("Hubo un total de " + venttot + " ventas");
+
+            Console.WriteLine("Las ganancias totales son de " + (venttot * 5500) + "$");
+
+            for (int i = 0; i < cant; i++)
+            {
+                if (a[i].idv != 0)
+                {
+                    Console.WriteLine("Nombre: " + a[i].nomv);
+                    Console.WriteLine("Apellido: " + a[i].apv);
+                    Console.WriteLine("ID: " + a[i].idv);
+                    Console.WriteLine("Ventas: " + a[i].vent);
+                    Console.WriteLine("Ganancias: " + (a[i].vent * 5500));
+                    Console.WriteLine("Horas Trabajadas: " + a[i].hr);
+                    Console.WriteLine("");
+                }
+                else
+                {
+                }
+
+            }
+            Console.ReadLine();
+
+            Console.Clear();
+
+            int mayor = a[0].vent;
+            int posmayor = 0;
+
+            for (int i = 0; i < cant; i++)
+            {
+                if (mayor < a[i].vent)
+                {
+                    mayor = a[i].vent;
+                    posmayor = i;
+                }
+            }
+            Console.WriteLine("El mayor vendendor es:");
+            Console.WriteLine("Nombre: " + a[posmayor].nomv);
+            Console.WriteLine("Apellido: " + a[posmayor].apv);
+            Console.WriteLine("ID: " + a[posmayor].idv);
+            Console.WriteLine("Ventas: " + a[posmayor].vent);
+            Console.WriteLine("Horas Trabajadas: " + a[posmayor].hr);
+        }
+
+        static void Main(string[] args)
+        {
+            int cant;
+            int op;
+
+            Console.WriteLine("Ingrese la cantidad de empleados");
+            cant = int.Parse(Console.ReadLine());
+            trabajadores[] a = new tr[cant];
+
+            info(a, cant);
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Ingrese la opcion a elegir");
+                Console.WriteLine("1-ordenar ambos sectores");
+                Console.WriteLine("2-el mejor empleado");
+                Console.WriteLine("3-ventas totales");
+                Console.WriteLine("4-Salir");
+
+                op = Convert.ToInt32(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        Console.Clear();
+                        OrdenarSector(a, cant);
+                        Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        MejorEmp(a, cant);
+                        Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        VentasTot(a, cant);
+                        Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("Usted eligio la opcion salir , toca una tecla para continuar ");
+                        Console.ReadLine();
+                        break;
+                }
+
+            } while (op != 4);
+        }
+    }
+}
